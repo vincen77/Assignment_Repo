5ba3c4a547a731702afc05e01e86514adf6dac36
vincen77 <72100032+vincen77@users.noreply.github.com>
2022-10-24 16:10:24 -0400

    Fixed errors

--- client/src/components/Modal.vue
+++ client/src/components/Modal.vue
@@ -13,23 +13,23 @@ const props = defineProps({
             <p>Please fill in this form to create an account.</p>
             <hr>
             <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" required>
+            <input class="input" type="text" placeholder="Enter Email" name="email" required>
 
             <label for="psw"><b>Password</b></label>
             <input type="password" placeholder="Enter Password" name="psw" required>
 
             <label for="psw-repeat"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+            <input class="input" type="password" placeholder="Repeat Password" name="psw-repeat" required>
             
             <label>
-                <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+                <input type="checkbox" checked name="remember" style="margin-bottom:15px"> Remember me
             </label>
 
             <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
 
             <div class="clearfix">
-                <button class="cancelbtn" @click="$emit('close')">Close</button>
-                <button type="submit" class="signupbtn">Coming soon</button>
+                <button class="button is-primary" @click="$emit('close')">Close</button>
+                <button type="submit" class="button is-danger">Coming soon</button>
             </div>
           </div>
         </div>
@@ -39,44 +39,6 @@ const props = defineProps({
 
 <style>
 
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-  border: none;
-  background: #f1f1f1;
-}
-
-input[type=text]:focus, input[type=password]:focus {
-  background-color: #ddd;
-  outline: none;
-}
-
-button {
-  background-color: #04AA6D;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-}
-
-button:hover {
-  opacity:1;
-}
-
-.cancelbtn {
-  padding: 14px 20px;
-  background-color: #f44336;
-}
-
-.cancelbtn, .signupbtn {
-  float: left;
-  width: 50%;
-}
 
 .modal-mask {
   position: fixed;
